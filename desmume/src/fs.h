8bef579526effcdb2428992c7522eb22fba1dbd4
twinaphex <libretro@gmail.com>
2015-08-10 12:46:56 +0200

    Convert fs-linux.c back to C

--- desmume/src/fs.h
+++ desmume/src/fs.h
@@ -19,25 +19,33 @@
 #ifndef FS_H
 #define FS_H
 
-#include "types.h"
+#include <stdint.h>
 
 #define FS_IS_DIR 1
 
 #define FS_ERR_UNKNOWN -1
 #define FS_ERR_NO_MORE_FILES 1
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern const char FS_SEPARATOR;
 
 typedef struct {
 	char cFileName[256];
 	char cAlternateFileName[14];
-	u32 flags;
-	u32 fileSize;
+	uint32_t flags;
+	uint32_t fileSize;
 } FsEntry;
 
 void * FsReadFirst(const char * path, FsEntry * entry);
 int FsReadNext(void * search, FsEntry * entry);
 void FsClose(void * search);
 int FsError(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
