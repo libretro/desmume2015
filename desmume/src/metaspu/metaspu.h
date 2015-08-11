4a7a0e650b89ad278e300582f830a5bdb00d5d25
twinaphex <libretro@gmail.com>
2015-08-11 16:56:39 +0200

    (metaspu) Simplify

--- desmume/src/metaspu/metaspu.h
+++ desmume/src/metaspu/metaspu.h
@@ -34,20 +34,14 @@ class ISynchronizingAudioBuffer
 {
 public:
 	ISynchronizingAudioBuffer()
-		: mixqueue_go(false)
-		,
-		#ifdef NDEBUG
-		adjustobuf(200,1000)
-		#else
-		adjustobuf(22000,44000)
-		#endif
+		: mixqueue_go(false), adjustobuf()
 	{
 
 	}
 
 	bool mixqueue_go;
 
-	virtual void enqueue_samples(s16* buf, int samples_provided)
+	void enqueue_samples(s16* buf, int samples_provided)
 	{
 		for(int i=0;i<samples_provided;i++) {
 			s16 left = *buf++;
@@ -57,27 +51,17 @@ public:
 	}
 
 	//returns the number of samples actually supplied, which may not match the number requested
-	virtual int output_samples(s16* buf, int samples_requested)
+	int output_samples(s16* buf, int samples_requested)
 	{
 		int done = 0;
-		if(!mixqueue_go) {
-			if(adjustobuf.size > 200)
-				mixqueue_go = true;
-		}
-		else
-		{
-			for(int i=0;i<samples_requested;i++) {
-				if(adjustobuf.size==0) {
-					mixqueue_go = false;
-					break;
-				}
-				done++;
-				s16 left, right;
-				adjustobuf.dequeue(left,right);
-				*buf++ = left;
-				*buf++ = right;
-			}
-		}
+      for(int i=0;i<samples_requested;i++)
+      {
+         done++;
+         s16 left, right;
+         adjustobuf.dequeue(left,right);
+         *buf++ = left;
+         *buf++ = right;
+      }
 		
 		return done;
 	}
@@ -86,26 +70,13 @@ private:
 	class Adjustobuf
 	{
 	public:
-		Adjustobuf(int _minLatency, int _maxLatency)
+		Adjustobuf()
 			: size(0)
-			, minLatency(_minLatency)
-			, maxLatency(_maxLatency)
 		{
-			rollingTotalSize = 0;
-			targetLatency = (maxLatency + minLatency)/2;
-			rate = 1.0f;
-			cursor = 0.0f;
-			curr[0] = curr[1] = 0;
-			kAverageSize = 80000;
 		}
 
-		float rate, cursor;
-		int minLatency, targetLatency, maxLatency;
 		std::queue<s16> buffer;
 		int size;
-		s16 curr[2];
-
-		std::queue<int> statsHistory;
 
 		void enqueue(s16 left, s16 right) 
 		{
@@ -114,55 +85,14 @@ private:
 			size++;
 		}
 
-		s64 rollingTotalSize;
-
-		u32 kAverageSize;
-
-		void addStatistic()
-		{
-			statsHistory.push(size);
-			rollingTotalSize += size;
-			if(statsHistory.size()>kAverageSize)
-			{
-				rollingTotalSize -= statsHistory.front();
-				statsHistory.pop();
-
-				float averageSize = (float)(rollingTotalSize / kAverageSize);
-				//static int ctr=0;  ctr++; if((ctr&127)==0) printf("avg size: %f curr size: %d rate: %f\n",averageSize,size,rate);
-				{
-					float targetRate;
-					if(averageSize < targetLatency)
-					{
-						targetRate = 1.0f - (targetLatency-averageSize)/kAverageSize;
-					}
-					else if(averageSize > targetLatency) {
-						targetRate = 1.0f + (averageSize-targetLatency)/kAverageSize;
-					} else targetRate = 1.0f;
-				
-					rate = targetRate;
-				}
-
-			}
-
-
-		}
-
 		void dequeue(s16& left, s16& right)
 		{
 			left = right = 0; 
-			addStatistic();
-			if(size==0) { return; }
-			cursor += rate;
-			while(cursor>1.0f) {
-				cursor -= 1.0f;
-				if(size>0) {
-					curr[0] = buffer.front(); buffer.pop();
-					curr[1] = buffer.front(); buffer.pop();
-					size--;
-				}
-			}
-			left = curr[0]; 
-			right = curr[1];
+			if(size==0)
+            return;
+         left = buffer.front(); buffer.pop();
+         right = buffer.front(); buffer.pop();
+         size--;
 		}
 	} adjustobuf;
 };
