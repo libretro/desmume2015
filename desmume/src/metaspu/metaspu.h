bac27cdf1bc89ca32cdf23214f4211d60daac5f5
twinaphex <libretro@gmail.com>
2015-08-11 16:32:30 +0200

    Leave only Zeromus' synchronizer in metaspu.cpp - long-term we want to get rid of this whole thing anyway

--- desmume/src/metaspu/metaspu.h
+++ desmume/src/metaspu/metaspu.h
@@ -25,35 +25,152 @@
 #define _METASPU_H_
 
 #include <algorithm>
+#include <queue>
+#include <assert.h>
 
 #include "types.h"
-
-template< typename T >
-static FORCEINLINE void Clampify( T& src, T min, T max )
-{
-	src = std::min( std::max( src, min ), max );
-}
-
-template< typename T >
-static FORCEINLINE T GetClamped( T src, T min, T max )
-{
-	return std::min( std::max( src, min ), max );
-}
 
 class ISynchronizingAudioBuffer
 {
 public:
-	virtual void enqueue_samples(s16* buf, int samples_provided) = 0;
+	ISynchronizingAudioBuffer()
+		: mixqueue_go(false)
+		,
+		#ifdef NDEBUG
+		adjustobuf(200,1000)
+		#else
+		adjustobuf(22000,44000)
+		#endif
+	{
+
+	}
+
+	bool mixqueue_go;
+
+	virtual void enqueue_samples(s16* buf, int samples_provided)
+	{
+		for(int i=0;i<samples_provided;i++) {
+			s16 left = *buf++;
+			s16 right = *buf++;
+			adjustobuf.enqueue(left,right);
+		}
+	}
 
 	//returns the number of samples actually supplied, which may not match the number requested
-	virtual int output_samples(s16* buf, int samples_requested) = 0;
+	virtual int output_samples(s16* buf, int samples_requested)
+	{
+		int done = 0;
+		if(!mixqueue_go) {
+			if(adjustobuf.size > 200)
+				mixqueue_go = true;
+		}
+		else
+		{
+			for(int i=0;i<samples_requested;i++) {
+				if(adjustobuf.size==0) {
+					mixqueue_go = false;
+					break;
+				}
+				done++;
+				s16 left, right;
+				adjustobuf.dequeue(left,right);
+				*buf++ = left;
+				*buf++ = right;
+			}
+		}
+		
+		return done;
+	}
+
+private:
+	class Adjustobuf
+	{
+	public:
+		Adjustobuf(int _minLatency, int _maxLatency)
+			: size(0)
+			, minLatency(_minLatency)
+			, maxLatency(_maxLatency)
+		{
+			rollingTotalSize = 0;
+			targetLatency = (maxLatency + minLatency)/2;
+			rate = 1.0f;
+			cursor = 0.0f;
+			curr[0] = curr[1] = 0;
+			kAverageSize = 80000;
+		}
+
+		float rate, cursor;
+		int minLatency, targetLatency, maxLatency;
+		std::queue<s16> buffer;
+		int size;
+		s16 curr[2];
+
+		std::queue<int> statsHistory;
+
+		void enqueue(s16 left, s16 right) 
+		{
+			buffer.push(left);
+			buffer.push(right);
+			size++;
+		}
+
+		s64 rollingTotalSize;
+
+		u32 kAverageSize;
+
+		void addStatistic()
+		{
+			statsHistory.push(size);
+			rollingTotalSize += size;
+			if(statsHistory.size()>kAverageSize)
+			{
+				rollingTotalSize -= statsHistory.front();
+				statsHistory.pop();
+
+				float averageSize = (float)(rollingTotalSize / kAverageSize);
+				//static int ctr=0;  ctr++; if((ctr&127)==0) printf("avg size: %f curr size: %d rate: %f\n",averageSize,size,rate);
+				{
+					float targetRate;
+					if(averageSize < targetLatency)
+					{
+						targetRate = 1.0f - (targetLatency-averageSize)/kAverageSize;
+					}
+					else if(averageSize > targetLatency) {
+						targetRate = 1.0f + (averageSize-targetLatency)/kAverageSize;
+					} else targetRate = 1.0f;
+				
+					rate = targetRate;
+				}
+
+			}
+
+
+		}
+
+		void dequeue(s16& left, s16& right)
+		{
+			left = right = 0; 
+			addStatistic();
+			if(size==0) { return; }
+			cursor += rate;
+			while(cursor>1.0f) {
+				cursor -= 1.0f;
+				if(size>0) {
+					curr[0] = buffer.front(); buffer.pop();
+					curr[1] = buffer.front(); buffer.pop();
+					size--;
+				}
+			}
+			left = curr[0]; 
+			right = curr[1];
+		}
+	} adjustobuf;
 };
 
 enum ESynchMethod
 {
 	ESynchMethod_N, //nitsuja's
 	ESynchMethod_Z, //zero's
-	ESynchMethod_P, //PCSX2 spu2-x
 };
 
 ISynchronizingAudioBuffer* metaspu_construct(ESynchMethod method);
