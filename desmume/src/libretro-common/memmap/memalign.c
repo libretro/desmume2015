2fb4b68732d77fffb48a2b17209dd36946b9b84b
twinaphex <libretro@gmail.com>
2016-08-19 09:42:11 +0200

    (libretro-common) Updates

--- desmume/src/libretro-common/memmap/memalign.c
+++ desmume/src/libretro-common/memmap/memalign.c
@@ -56,6 +56,8 @@ void *memalign_alloc_aligned(size_t size)
 {
 #if defined(__x86_64__) || defined(__LP64) || defined(__IA64__) || defined(_M_X64) || defined(_WIN64)
    return memalign_alloc(64, size);
+#elif defined(__i386__) || defined(__i486__) || defined(__i686__) || defined(GEKKO)
+   return memalign_alloc(32, size);
 #else
    return memalign_alloc(32, size);
 #endif
