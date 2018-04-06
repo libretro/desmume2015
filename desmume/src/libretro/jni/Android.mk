54d83a62557304ef70739db216fe93ae473f47d3
Aaron Kling <webgeek1234@gmail.com>
2018-04-05 20:43:36 -0500

    Refactor android jni makefiles

--- desmume/src/libretro/jni/Android.mk
+++ desmume/src/libretro/jni/Android.mk
@@ -1,53 +1,36 @@
-HAVE_GRIFFIN    := 0
-
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+CORE_DIR := $(LOCAL_PATH)/../..
+
+JIT             :=
+DESMUME_JIT     := 0
+DESMUME_JIT_ARM := 0
+
+ifeq ($(TARGET_ARCH),arm)
+  DESMUME_JIT_ARM := 1
+  JIT             := -DHAVE_JIT
+endif
+
+ifneq (,$(filter $(TARGET_ARCH),x86 x86_64))
+  DESMUME_JIT := 1
+  JIT         := -DHAVE_JIT
+endif
+
+include $(CORE_DIR)/../Makefile.common
+
+COREFLAGS :=  $(JIT) $(INCDIR) -D__LIBRETRO__ -Wno-write-strings -DANDROID -DFRONTEND_SUPPORTS_RGB565
 
 GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
 ifneq ($(GIT_VERSION)," unknown")
-	LOCAL_CXXFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+  COREFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
 endif
 
-CORE_DIR = ../..
-JIT=
-DESMUME_JIT=0
-DESMUME_JIT_ARM=0
-
-ifeq ($(TARGET_ARCH),arm)
-LOCAL_CXXFLAGS := -DANDROID_ARM
-LOCAL_ARM_MODE := arm
-
-DESMUME_JIT_ARM = 1
-JIT = -D__RETRO_ARM__ -DHAVE_JIT
-endif
-
-ifeq ($(TARGET_ARCH),x86)
-LOCAL_CXXFLAGS :=  -DANDROID_X86
-DESMUME_JIT = 1
-JIT = -DHAVE_JIT
-endif
-
-ifeq ($(TARGET_ARCH),mips)
-LOCAL_CXXFLAGS := -DANDROID_MIPS -D__mips__ -D__MIPSEL__
-endif
-
-ifeq ($(TARGET_ARCH),mips64)
-LOCAL_CXXFLAGS := -DANDROID_MIPS -D__mips__ -D__MIPSEL__
-endif
-
-LOCAL_MODULE    := libretro
-
-include ../../../Makefile.common
-
-LOCAL_SRC_FILES := $(SOURCES_CXX) $(SOURCES_C)
-GLOBAL_DEFINES :=  $(JIT) -fexceptions
-
-LOCAL_CXXFLAGS += -O3 -D__LIBRETRO__ -Wno-write-strings -DANDROID -DFRONTEND_SUPPORTS_RGB565 $(GLOBAL_DEFINES) $(INCDIR)
-LOCAL_CFLAGS = -O3 -D__LIBRETRO__ -Wno-psabi -Wno-write-strings -DANDROID -DFRONTEND_SUPPORTS_RGB565 $(GLOBAL_DEFINES) $(INCDIR)
-
-LOCAL_C_INCLUDES = -I$(CORE_DIR)/libretro/zlib -iquote $(CORE_DIR) -iquote $(CORE_DIR)/libretro
-
-LOCAL_LDLIBS += -lz -latomic
-
+include $(CLEAR_VARS)
+LOCAL_MODULE       := retro
+LOCAL_SRC_FILES    := $(SOURCES_CXX) $(SOURCES_C)
+LOCAL_CXXFLAGS     := $(COREFLAGS)
+LOCAL_CFLAGS       := $(COREFLAGS)
+LOCAL_LDFLAGS      := -Wl,-version-script=$(CORE_DIR)/libretro/link.T
+LOCAL_LDLIBS       := -lz
+LOCAL_CPP_FEATURES := exceptions
 include $(BUILD_SHARED_LIBRARY)
